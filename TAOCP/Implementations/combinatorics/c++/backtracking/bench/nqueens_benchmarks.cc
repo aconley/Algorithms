622fa5e0f716968ffbb7cade72a40e736a3cb88d
Alexander Conley <alexanderconley@gmail.com>
2016-09-07 19:15:39 -0600

    Basic benchmarks for nqueens

---
+++ TAOCP/Implementations/combinatorics/c++/backtracking/bench/nqueens_benchmarks.cc
@@ -0,0 +1,41 @@
+#include "benchmark/benchmark.h"
+#include "nqueens.h"
+
+template<std::size_t n> class CountingVisitor {
+  private:
+    int nsolutions;
+
+  public:
+    CountingVisitor() { nsolutions = 0; }
+
+    bool visit(const std::array<int, n>& rows) {
+      ++nsolutions;
+      return true;
+    }
+
+    void reset() {
+      nsolutions = 0;
+    }
+
+    int getN() const {
+      return nsolutions;
+    }
+};
+
+static void BM_Nqueens_Basic8(benchmark::State& state) {
+  CountingVisitor<8> v;
+  while (state.KeepRunning()) {
+    backtracking::nqueens_basic(v);
+  }
+}
+BENCHMARK(BM_Nqueens_Basic8);
+
+static void BM_Nqueens_Basic12(benchmark::State& state) {
+  CountingVisitor<12> v;
+  while (state.KeepRunning()) {
+    backtracking::nqueens_basic(v);
+  }
+}
+BENCHMARK(BM_Nqueens_Basic12);
+
+BENCHMARK_MAIN();
