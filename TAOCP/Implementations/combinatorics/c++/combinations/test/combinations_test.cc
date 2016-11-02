9bd811c4db0db9414238be3377e993f6e9e7d59f
Alexander Conley <alexanderconley@gmail.com>
2016-11-01 18:33:04 -0600

    Fix (0,3) combinations test.

--- TAOCP/Implementations/combinatorics/c++/combinations/test/combinations_test.cc
+++ TAOCP/Implementations/combinatorics/c++/combinations/test/combinations_test.cc
@@ -3,13 +3,13 @@
 #include "gtest/gtest.h"
 #include "combinations.h"
 
-template<std::size_t n> class RecordingVisitor {
+template<std::size_t t> class RecordingVisitor {
   private:
-    std::vector<std::array<int, n>> solutions;
+    std::vector<std::array<int, t>> solutions;
 
   public:
-    bool visit(const std::array<int, n>& rows) {
-      std::array<int, n> v(rows);
+    bool visit(const std::array<int, t>& rows) {
+      std::array<int, t> v(rows);
       solutions.push_back(v);
       return true;
     }
@@ -22,19 +22,19 @@ template<std::size_t n> class RecordingVisitor {
       return solutions.size();
     }
 
-    const std::array<int, n>& get(int i) {
+    const std::array<int, t>& get(int i) {
       return solutions.at(i);
     }
 };
 
-template<std::size_t n> class CountingVisitor {
+template<std::size_t t> class CountingVisitor {
   private:
     int nsolutions;
 
   public:
     CountingVisitor() { nsolutions = 0; }
 
-    bool visit(const std::array<int, n>& rows) {
+    bool visit(const std::array<int, t>& rows) {
       ++nsolutions;
       return true;
     }
@@ -50,7 +50,7 @@ template<std::size_t n> class CountingVisitor {
 
 TEST(CombinationsTest, TestAll) {
   RecordingVisitor<3> vis;
-  combinations::combinations_lex_basic(3, vis);
+  combinations::combinations_lex_basic(0, vis);
   std::array<int, 3> expected = {{0, 1, 2}};
   EXPECT_EQ(vis.getN(), 1) << "Should have 1 visit for (0, 3) visitor";
   EXPECT_EQ(vis.get(0), expected) << "Didn't get expected indices";
