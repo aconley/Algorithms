0d09f0204caa2d3ee1f347e733e8ae478020b8a4
Alexander Conley <alexanderconley@gmail.com>
2017-01-16 14:40:57 -0700

    Add combinations benchmark notes

--- TAOCP/Implementations/combinatorics/c++/combinations/bench/combinations_bench.cc
+++ TAOCP/Implementations/combinatorics/c++/combinations/bench/combinations_bench.cc
@@ -56,4 +56,21 @@ static void BM_Combinations_16_5(benchmark::State& state) {
 }
 BENCHMARK(BM_Combinations_16_5);
 
+// Grey code / Revolving door
+static void BM_Combinations_Grey_14_4(benchmark::State& state) {
+  CountingVisitor<4> vis4;
+  while (state.KeepRunning()) {
+    combinations::combinations_grey(14, vis4);
+  }
+}
+BENCHMARK(BM_Combinations_Grey_14_4);
+
+static void BM_Combinations_Grey_16_5(benchmark::State& state) {
+  CountingVisitor<5> vis5;
+  while (state.KeepRunning()) {
+    combinations::combinations_grey(16, vis5);
+  }
+}
+BENCHMARK(BM_Combinations_Grey_16_5);
+
 BENCHMARK_MAIN();
