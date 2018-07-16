6cb08cad429e85fc47d5b9ab622f27d6c8595382
Alex Conley <alexanderconley@gmail.com>
2018-07-15 20:12:56 -0600

    Initial rust nqueens implementation

--- TAOCP/Implementations/combinatorics/backtracking/rust/src/lib.rs
+++ TAOCP/Implementations/combinatorics/backtracking/rust/src/lib.rs
@@ -7,6 +7,24 @@ pub struct Solution {
     rows: Vec<u8>,
 }
 
+// A function which visits a solution, returning false if
+// no further iterations are desired. 
+pub type Visitor = Fn(&Solution) -> bool;
+
+pub trait NQueensSolver {
+    fn visit(&mut self, n: u8, v: &Visitor);
+}
+
+pub struct BitwiseNQueensSolver {
+    n: usize,
+    nm1: usize, 
+    a: u64,
+    b: u64, 
+    c: u64,
+    s: Solution,
+    done: bool
+}
+
 impl fmt::Display for Solution {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         let mut board = vec![vec![false; self.n as usize]; self.n as usize];
@@ -30,6 +48,59 @@ impl fmt::Display for Solution {
     }
 }
 
+impl BitwiseNQueensSolver {
+    fn init(&mut self, n: u8) -> () {
+        self.a = 0;
+        self.b = 0;
+        self.c = 0;
+        self.n = n as usize;
+        self.nm1 = (n - 1) as usize;
+        self.done = false;
+        self.s = Solution {n : n, rows: vec![0; n as usize]};
+    }
+
+    fn visit_levels(&mut self, l: usize, v: &Visitor) {
+        if self.done {
+            return;
+        }
+        if l > self.nm1 {
+            if !v(&self.s) {
+                self.done = true;
+                return;
+            }
+        }
+
+        for xl in 0..self.n {
+            let xla : u64 = 1u64 << xl;
+            let xlb : u64 = xla << l;
+            let xlc : u64 = 1u64 << (xl - l + self.nm1);
+            if (self.a & xla == 0) 
+                && (self.b & xlb == 0) 
+                && (self.c & xlc == 0) {
+
+                // Valid candidate x_l
+                self.s.rows[l] = xl as u8;
+                self.a |= xla;
+                self.b |= xlb;
+                self.c |= xlc;
+
+                self.visit_levels(l + 1, v);
+
+                // Undo. 
+                self.a &= !xla;
+                self.b &= !xlb;
+                self.c &= !xlc;
+            }
+        }
+    }
+}
+
+impl NQueensSolver for BitwiseNQueensSolver {
+    fn visit(&mut self, n: u8, v: &Visitor) {
+        self.init(n);
+        self.visit_levels(0, v);
+    }
+}
 
 #[cfg(test)]
 mod tests {
