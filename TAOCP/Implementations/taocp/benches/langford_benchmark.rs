e650ef6a2f63d9ba3da94681f1006cc6534cbf11
Alex Conley <alexanderconley@gmail.com>
2021-12-19 08:48:54 -0700

    Add langford 8 test.

--- TAOCP/Implementations/taocp/benches/langford_benchmark.rs
+++ TAOCP/Implementations/taocp/benches/langford_benchmark.rs
@@ -11,11 +11,22 @@ fn langford7_benchmark(c: &mut Criterion) {
   });
 }
 
+fn langford8_benchmark(c: &mut Criterion) {
+  c.bench_function("Langford 8", |b| {
+    b.iter(|| LangfordIterator::new(8).count())
+  });
+}
+
 fn langford12_benchmark(c: &mut Criterion) {
   c.bench_function("Langford 12", |b| {
     b.iter(|| LangfordIterator::new(12).count())
   });
 }
 
-criterion_group!(benches, langford7_benchmark, langford12_benchmark);
+criterion_group!(
+  benches,
+  langford7_benchmark,
+  langford8_benchmark,
+  langford12_benchmark
+);
 criterion_main!(benches);
