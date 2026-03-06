d2ba22e0ad0ba55db2850f497cb5c68fa4726e94
Alex Conley <alexanderconley@gmail.com>
2026-03-06 09:54:08 -0700

    Codex implementations of generating waerden and langford SAT problems.

--- src/sat/mod.rs
+++ src/sat/mod.rs
@@ -1,7 +1,11 @@
 pub mod backtracking;
 pub mod lazy_backtracking;
+pub mod sample_problems;
 pub mod sat_problem;
 
 pub use backtracking::solve_via_backtracking;
 pub use lazy_backtracking::solve_via_lazy_backtracking;
+pub use sample_problems::{
+    langford, langford_solution_arrangement, waerden, waerden_solution_string, SampleProblemError,
+};
 pub use sat_problem::{Clause, ClauseError, SatProblem, SatProblemError};
